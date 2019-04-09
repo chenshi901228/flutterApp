8fa40ab94d149731b2a28925a1b0e6be51405b62
chenshi <756705113@qq.com>
2019-04-09 16:22:21 +0800

    首页改回CustomScrollView

--- flutterapp/lib/component/home/tabItem1.dart
+++ flutterapp/lib/component/home/tabItem1.dart
@@ -63,36 +63,40 @@ class _TabItem1State extends State<TabItem1Component>
               height: ScreenUtil().setWidth(32),
             ),
           )),
-      body: ListView(
+      body: CustomScrollView(
         controller: _scrollController,
-        children: <Widget>[
-          new SwiperComponent(
-            data: _this["bannerImgsList"],
-          ),
-          new ClassifyComponent(
-            data: _this["classifyList"],
-          ),
-          new LastTimeComponent(),
-          new GoodsItemComponent(
-            data: _this["goodsList"]?.sublist(0, 4),
-          ),
-          new MustBuyComponent(
-            data: _this["goodsList"]?.sublist(4, 8),
-          ),
-          new GoodsItemComponent(
-            title: "每日必逛",
-            data: _this["goodsList"]?.sublist(8, 12),
-          ),
-          new GoodsItemComponent(
-            title: "潮玩推荐",
-            data: _this["goodsList"]?.sublist(4, 8),
-          ),
-          new GoodsItemComponent(
-            title: "品质生活",
-            data: _this["goodsList"]?.sublist(8, 12),
-          ),
-          new GuessYourComponent(
-            data: _this["goodsList"],
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate([
+              new SwiperComponent(
+                data: _this["bannerImgsList"],
+              ),
+              new ClassifyComponent(
+                data: _this["classifyList"],
+              ),
+              new LastTimeComponent(),
+              new GoodsItemComponent(
+                data: _this["goodsList"]?.sublist(0, 4),
+              ),
+              new MustBuyComponent(
+                data: _this["goodsList"]?.sublist(4, 8),
+              ),
+              new GoodsItemComponent(
+                title: "每日必逛",
+                data: _this["goodsList"]?.sublist(8, 12),
+              ),
+              new GoodsItemComponent(
+                title: "潮玩推荐",
+                data: _this["goodsList"]?.sublist(4, 8),
+              ),
+              new GoodsItemComponent(
+                title: "品质生活",
+                data: _this["goodsList"]?.sublist(8, 12),
+              ),
+              new GuessYourComponent(
+                data: _this["goodsList"],
+              )
+            ]),
           )
         ],
       ),
